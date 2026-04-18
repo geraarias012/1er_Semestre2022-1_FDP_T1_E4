4b12d002a1afad9f0135a5d66292a759467487d7
Gerardo Arias <91097929+geraarias012@users.noreply.github.com>
2026-04-18 07:49:07 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float A,B;
+    printf("Introduzca el valor de A: ");
+    scanf("%f", &A);
+    printf("Introduzca el valor de B: ");
+    scanf("%f", &B);
+    printf("El valor de A es %f\n", B);
+    printf("El valor de B es %f", A);
+    return 0;
+}
